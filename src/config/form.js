7ddc9ff9cba357f9a76eb3eb2c03b24bd223dbd6
Krittamark <markkrit41@gmail.com>
2023-03-30 16:23:27 +0700

    Add printout form

--- src/config/form.js
+++ src/config/form.js
@@ -2,10 +2,14 @@ import { workshopForm } from "./forms/workshopForm";
 import { emeeting } from "./forms/emeeting";
 import { serviceProviderBasic } from "./forms/serviceProviderBasic";
 import { serviceProviderAdvance } from "./forms/serviceProviderAdvance";
+import { printoutPartner } from "./forms/printoutPartner";
+import { printoutPrimary } from "./forms/printoutPrimary";
 
 export {
 	workshopForm,
 	emeeting,
 	serviceProviderBasic,
 	serviceProviderAdvance,
+	printoutPartner,
+	printoutPrimary,
 }
